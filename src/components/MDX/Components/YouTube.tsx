6784cdbdd81a74ec8acd449c99eeaca170c15b8c
Rock Star <rohitkumarrohit96@gmail.com>
2024-01-07 12:49:03 +0530

    Update the cursor color of the theme switcher.

--- src/components/MDX/Components/YouTube.tsx
+++ src/components/MDX/Components/YouTube.tsx
@@ -4,7 +4,7 @@ import { Text } from "@radix-ui/themes";
 
 export default function YouTube({ id, title }: { id: string, title?: string }) {
   return (
-    <>
+    <main className="border border-border p-1 rounded-md">
     <div className="max-w-full overflow-hidden relative pb-[56.25%] h-0 rounded-md">
           <iframe
               className="absolute top-0 left-0 w-full h-full"
@@ -15,6 +15,6 @@ export default function YouTube({ id, title }: { id: string, title?: string }) {
           ></iframe>
       </div>
       <Text size="5" weight="bold" align="center" className="line-clamp-1">{title}</Text>
-      </>
+      </main>
   );
 }
