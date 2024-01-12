c07ca7cb7e0928ce16b2387e0d2309eb4e8e0e0b
Rock Star <rohitkumarrohit96@gmail.com>
2024-01-13 03:17:47 +0530

    Update image source to use fallback if not available

--- src/components/FolderListLayout.tsx/ImageCard.tsx
+++ src/components/FolderListLayout.tsx/ImageCard.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Card, Image } from "@nextui-org/react";
+import { siteConfig } from "®/config/site";
 
 const ImageCard = () => {
 return (
         <Card className="FolderCard" shadow="none" isHoverable isPressable>
             <div className="h-40 flex justify-center items-center">
                 <Image 
-                    src="https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1666344481.20013313.png" 
+                    src={"https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1666344481.20013313.png" || siteConfig.fallback}
                     alt="Xiaomi-12-Pro-(Dimensity-Edition)"  
                     className="mx-auto my-10 h-48 object-center object-contain rounded-none"
                     isBlurred
