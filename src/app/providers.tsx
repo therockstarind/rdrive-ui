f847fbc788d5bc03bd2ac19e7ea112973daa420a
Rock Star <rohitkumarrohit96@gmail.com>
2024-01-26 04:04:18 +0530

    toc update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -17,18 +17,15 @@ export function Providers({children}: { children: React.ReactNode }) {
       <NextUIProvider navigate={router.push}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Theme>
-            <div vaul-drawer-wrapper="">
               <div className="flex min-h-dvh flex-col items-center justify-center bg-background">
                   <main className="flex w-full flex-1 flex-col">
                     <div className="mx-auto w-full max-w-7xl p-2 my-2 overflow-clip">
-
                       {children}
                     </div>
                   </main>
                 <Footer />
                 <AppBar />
               </div>
-            </div>
             <Toaster />
             </Theme>
         </ThemeProvider>
--- src/components/blog/blogtoc.tsx
+++ src/components/blog/blogtoc.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import { Button, Listbox, ListboxItem } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
 import { LuTextQuote } from "react-icons/lu";
-import { useMediaQuery } from "®/hooks/use-media-query";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "®ui/command";
 import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "®ui/dialog";
-import { Drawer, DrawerContent, DrawerTrigger } from "®ui/drawer";
-import Tooltip from "®ui/tooltip"
+import Tooltip from "®ui/tooltip";
 
 type TocItem = {
   id: string;
@@ -25,78 +23,34 @@ const TocList: FC<TocListProps> = ({ toc, onNavigate }) => {
   return (
       <Command>
           <CommandInput placeholder="Table of Content"/>
-          <Listbox variant="light" className="p-0">
-              <ListboxItem key="" className="p-0">
                   <CommandList className="border-t border-border">
                           <CommandEmpty>No content found.</CommandEmpty>
                       <CommandGroup heading="On this page">
                           {toc.map((item) => (
                               <CommandItem key={item.id} onSelect={() => onNavigate(item.id)}
                                   className={`${
-                                    item.level === 1 ? 'font-bold text-xl' : 
-                                    item.level === 2 ? 'pl-8 text-muted-foreground' : 
-                                    item.level === 3 ? 'pl-12 text-muted-foreground' :
-                                    'pl-16 text-muted-foreground'
+                                    item.level === 1 ? 'font-bold text-lg': 
+                                    item.level === 2 ? 'pl-6': 
+                                    item.level === 3 ? 'pl-8 line-clamp-1':
+                                    'pl-10'
                                 }`}
                               >
-                                  {item.title}
+                                  <div className="line-clamp-1">{item.title}</div>
                               </CommandItem>
                           ))}
                       </CommandGroup>
                   </CommandList>
-              </ListboxItem>
-          </Listbox>
       </Command>
   );
 };
 
-
-type DesktopTocProps = {
+type TocProps = {
   toc: TocItem[];
-  onNavigate: (id: string) => void;
+  onNavigate?: (id: string) => void;
 };
 
-const DesktopToc: FC<DesktopTocProps> = ({ toc, onNavigate }) => (
-  <>
-  <Tooltip content="Table of Content">
-    <DialogTrigger asChild>
-      <Button size="sm" isIconOnly radius="full" variant="light" className="border border-border bg-default/20 dark:bg-default/40">
-        <LuTextQuote size={20} />
-      </Button>
-    </DialogTrigger>
-  </Tooltip> 
-  <DialogContent className="max-w-2xl p-0 gap-0">
-      <DialogHeader />
-          <TocList toc={toc} onNavigate={onNavigate} />
-    </DialogContent></>
-);
-
-type MobileTocProps = {
-  toc: TocItem[];
-  onNavigate: (id: string) => void;
-};
-
-const MobileToc: FC<MobileTocProps> = ({ toc, onNavigate }) => (
-  <>
-  <Tooltip content="Table of Content">
-    <DrawerTrigger asChild>
-      <Button size="sm" isIconOnly radius="full" variant="light" className="border border-border bg-default/20 dark:bg-default/40">
-        <LuTextQuote size={20} />
-      </Button>
-    </DrawerTrigger>
-  </Tooltip>
-  <DrawerContent>
-          <TocList toc={toc} onNavigate={onNavigate} />
-    </DrawerContent></>
-);
-
-type BlogTocProps = {
-  toc: TocItem[];
-};
-
-const BlogToc: FC<BlogTocProps> = ({ toc }) => {
+const BlogToc: FC<TocProps> = ({ toc }) => {
   const [open, setOpen] = useState(false);
-  const isDesktop = useMediaQuery("(min-width: 768px)");
   const router = useRouter();
 
   const onNavigate = (id: string) => {
@@ -104,15 +58,21 @@ const BlogToc: FC<BlogTocProps> = ({ toc }) => {
       router.push(`#${id}`);
   };
 
-  return isDesktop ? (
+  return (
       <Dialog open={open} onOpenChange={setOpen}>
-          <DesktopToc toc={toc} onNavigate={onNavigate} />
+          <Tooltip content="Table of Content">
+    <DialogTrigger asChild>
+      <Button size="sm" isIconOnly radius="full" variant="light" className="border border-border bg-default/20 dark:bg-default/40">
+        <LuTextQuote size={20} />
+      </Button>
+    </DialogTrigger>
+  </Tooltip> 
+  <DialogContent className="max-w-2xl p-0 gap-0 translate-y-0 bottom-0 sm:translate-y-[-50%] sm:bottom-none">
+      <DialogHeader />
+          <TocList toc={toc} onNavigate={onNavigate} />
+    </DialogContent>
       </Dialog>
-  ) : (
-      <Drawer open={open} onOpenChange={setOpen}>
-          <MobileToc toc={toc} onNavigate={onNavigate} />
-      </Drawer>
-  );
+  )
 };
 
 export default BlogToc;
