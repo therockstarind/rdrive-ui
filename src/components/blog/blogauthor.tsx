f9ddd2d87b0f0645c44576ca315fb99ecb0d7ef7
Rock Star <rohitkumarrohit96@gmail.com>
2024-01-27 11:26:11 +0530

    Update blog author component with NextUI Navbar

--- src/components/blog/blogauthor.tsx
+++ src/components/blog/blogauthor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button } from "@nextui-org/react";
+import { Button, Navbar } from "@nextui-org/react";
 import { Avatar } from "@radix-ui/themes";
 import React from "react";
 import { BiSolidEdit } from "react-icons/bi";
@@ -14,7 +14,7 @@ type BlogAuthorProps = {
 
 const BlogAuthor: React.FC<BlogAuthorProps> = ({ toc }) => {
     return (
-        <div className="flex justify-between items-center sticky top-0 z-50 bg-background p-2 px-0 sm:px-2 border-b border-border">
+        <Navbar shouldHideOnScroll isBordered classNames={{wrapper:'px-0'}}>
             <div className="flex gap-2 items-center">         
             <Avatar radius="full" src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop" alt="User profile picture" fallback="T" />
                 <div className="-space-y-0.5 sm:-space-y-1">
@@ -24,7 +24,6 @@ const BlogAuthor: React.FC<BlogAuthorProps> = ({ toc }) => {
             </div>
             <div className="flex gap-2 items-center">
                 {toc}
-
                 {/* This will only show when user login with it self */}
                 <Tooltip content="Edit Post">
                     <Button isIconOnly size="sm" radius="full" variant="light" className="border border-border bg-default/20 dark:bg-default/40">
@@ -36,14 +35,13 @@ const BlogAuthor: React.FC<BlogAuthorProps> = ({ toc }) => {
                         <MdAddCircle size={20} />
                     </Button>
                 </Tooltip>
-
                 {/*
                     <Button size="sm" radius="full" variant="light" className="border border-border bg-default/20 dark:bg-default/40">
                             Follow
                     </Button> 
                     */}
             </div>
-        </div>
+        </Navbar>
     )
 }
 
