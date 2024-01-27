1fcf742a81c393b8697673a9dbcb4bf56e35c113
Rock Star <rohitkumarrohit96@gmail.com>
2024-01-28 03:06:45 +0530

    Update user profile page design and add social media links

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,7 +12,12 @@ export default function NotFound() {
             <h1 className="text-5xl font-bold" >Page not Found</h1>
             <p className="text-muted-foreground font-mono">Sorry, but we can't find that page or it has been deleted or removed</p>
             </div>
-            <Button variant="bordered" className="w-full max-w-48" onPress={() => router.push('/')}>BACK TO HOME</Button>
+            <button onClick={() => router.push('/')} className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-background px-6 py-0.5 text-sm backdrop-blur-3xl">
+              BACK TO HOME
+              </span>
+            </button>
         </div>
       </main>
   )
--- src/components/user/user.tsx
+++ src/components/user/user.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { Image } from "@nextui-org/react"
-import { Avatar, Flex, Separator, Text } from "@radix-ui/themes"
-import { FadeContainer } from "®/lib/FramerMotionVariants"
+import { Button, Image } from "@nextui-org/react"
+import { AspectRatio, Avatar, Flex, Separator, Text } from "@radix-ui/themes"
+import { FadeContainer, fromLeftVariant } from "®/lib/FramerMotionVariants"
 import AnimatedDiv from "../FramerMotion/AnimatedDiv"
+import { motion } from "framer-motion"
 
 const User = () => {
     return (
@@ -10,48 +11,62 @@ const User = () => {
             {/* Container */}
             <Flex direction={{ initial: "column", sm: "row" }} gap="4">
                 {/* User */}
-                <Flex className="md:w-2/4" direction="column" gap="3">
-                <div className="relative rounded-lg mx-auto">
-                        <div className="rounded-b-[50px] sm:rounded-md overflow-hidden opacity-90">
+                <Flex className="md:w-1/3" direction="column" gap="3">
+                <div className="relative">
+                        <AspectRatio ratio={16 / 9} className="rounded-b-[50px] sm:rounded-md overflow-hidden opacity-90">
                             <Image 
                                 src="/images/lyra.png" 
                                 alt="Cover Image" 
                                 className="rounded-none object-cover" 
+                                width="100%"
+                                height="100%"
                             />
-                        </div>
+                        </AspectRatio>
                         <Avatar 
-                            size="9" 
-                            src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg" 
+                            size="8" 
+                            src="/icons/rdrive.png" 
                             fallback="R"
                             className="absolute -bottom-14 left-1/2 transform -translate-x-1/2 rounded-full sm:rounded-md"   
                         />
                 </div>
-                <div className="mt-16 text-center">
-                        <h3 className="text-lg font-semibold">Username</h3>
-                        <p className="text-gray-600">@username</p>
-                </div>
-                <div>                   
-                </div>
+                <Flex direction="column" align="center" mt="8">
+                        <Text as="div" size="5" weight="bold">Rock Star</Text>
+                        <Text as="p" size="2" color="gray">@rockstar</Text>
+                </Flex>
+                <div />
                 {/* Counts */}
                 <div className="flex justify-around items-center">
                     <Flex direction="column" align="center" gap="1">
-                        <Text as="p" size="8" weight="bold">300</Text>
-                        <Text as="p" size="2" color="gray">Post</Text>
+                        <Text as="div" size="5" weight="bold">300</Text>
+                        <Text as="p" size="1" color="gray">Post</Text>
                     </Flex>
                     <Separator className="h-8" orientation="vertical" />
                     <Flex direction="column" align="center" gap="1">
-                        <Text as="p" size="8" weight="bold">200</Text>
-                        <Text as="p" size="2" color="gray">Followers</Text>
+                        <Text as="div" size="5" weight="bold">200</Text>
+                        <Text as="p" size="1" color="gray">Followers</Text>
                     </Flex>
                     <Separator className="h-8" orientation="vertical" />
                     <Flex direction="column" align="center" gap="1">
-                        <Text as="p" size="8" weight="bold">0</Text>
-                        <Text as="p" size="2" color="gray">Following</Text>
+                        <Text as="div" size="5" weight="bold">0</Text>
+                        <Text as="p" size="1" color="gray">Following</Text>
                     </Flex>
                 </div>
+                <div />
+                    {/* This button will be follow or edit based on user */}
+                <Button radius="full" variant="light" className="mx-4 border border-border bg-default/20 dark:bg-default/40">
+                    Follow
+                </Button> 
+                <div />
+                <Flex gap="2" mx="4" className="overflow-x-auto">
+                {social.map((social, index) => (
+                    <motion.div key={index} variants={fromLeftVariant}>
+                    <Button isIconOnly size="lg" variant="light" href={social.link}><Image src={social.icon} alt={social.name} /></Button>
+                    </motion.div>
+                ))}
+                </Flex>
                 </Flex>
                 {/* Card */}
-                <Flex className="md:w-3/4" direction="column" gap="3" p={{initial: '2', sm:'0'}}>
+                <Flex className="md:w-2/3" direction="column" gap="3" p={{initial: '2', sm:'0'}}>
                     <div className="grid md:grid-cols-2 gap-3">
                         {Array.from({ length: 5 }, (_, index) => (
                             <div key={index} className={`rounded-md h-40 bg-gradient-to-r ${index % 2 === 0 ? 'from-cyan-500 to-blue-500' : 'from-indigo-500 via-purple-500 to-pink-500'}`}></div>
@@ -64,3 +79,13 @@ const User = () => {
 };
 
 export default User;
+
+const social = [
+    {name:"Github", link:"https://github.com", icon:"/social/whatsapp.png"},
+    {name:"YouTube", link:"https://youtube.com", icon:"/social/youtube.png"},
+    {name:"X", link:"https://x.com", icon:"/social/x.png"},
+    {name:"WhatsApp", link:"https://whatsapp.com", icon:"/social/whatsapp.png"},
+    {name:"Discord", link:"https://discord.com", icon:"/social/discord.png"},
+    {name:"Github", link:"https://github.com", icon:"/social/github.png"},
+    {name:"Telegram", link:"https://telegram.com", icon:"/social/telegram.png"},
+]
